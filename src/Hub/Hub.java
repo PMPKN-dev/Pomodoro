2c1de8a251b69032602172ef5a3d7d867fbb9d8c
PMPKN-dev <michal-janczak@outlook.com>
2022-05-16 11:37:17 +0200

    super secret stuff, yknow

--- src/Hub/Hub.java
+++ src/Hub/Hub.java
@@ -1,4 +1,18 @@
 package Hub;
 
-public class Hub {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Hub extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Hub/Pomodoro/View/Pomodoro.fxml"));
+        Parent parent = fxmlLoader.load();
+        Scene scene = new Scene(parent,600,400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
