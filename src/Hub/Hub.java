d906f4b5a6d15e1a09430f02e984af723ff47ddb
PMPKN-dev <michal-janczak@outlook.com>
2022-05-16 11:59:41 +0200

    some layout for the Hub

--- src/Hub/Hub.java
+++ src/Hub/Hub.java
@@ -1,18 +1,34 @@
 package Hub;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.application.*;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.scene.canvas.*;
+import javafx.stage.*;
 
 public class Hub extends Application {
+
+    @FXML
+    Canvas GridCanvasPomodoro;
+    @FXML
+    Canvas GridCanvasDoToday;
+    @FXML
+    Canvas GridCanvasOfficeOverview;
+    @FXML
+    Canvas GridCanvasAdmin;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Hub/Pomodoro/View/Pomodoro.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Hub/Hub.fxml"));
         Parent parent = fxmlLoader.load();
-        Scene scene = new Scene(parent,600,400);
+        Scene scene = new Scene(parent,400,200);
         primaryStage.setScene(scene);
+        primaryStage.setTitle("Crazy name you won't believe");
         primaryStage.show();
+
+        //TODO, make the 4 grids render icons for the programs and add onClick launching for the programs
+        //TODO, (will require popups to run the fxml files of the other programs)
+
+
     }
 }
