d4a1ec4f7d9b5aab539cb545c5527add6dd0244a
Larskan <101105018+Larskan@users.noreply.github.com>
2022-05-26 14:35:50 +0200

    Cleaned it a bit

--- src/TheTomatoCo/Pomodoro/Controller/Pomodoro.java
+++ src/TheTomatoCo/Pomodoro/Controller/Pomodoro.java
@@ -17,6 +17,8 @@ public class Pomodoro extends Program {
 
     //private class constant and variables
     private static final Integer startTime = 15; //Placeholder until I figure out min:sec
+    //TODO: create a binding to a Label that updates the Label text as the chosen Min:sec
+    //TODO: make the chosen Min:Sec part of what user can edit
     private Timeline timeline;
     private final Label timerLabel = new Label();
     private final IntegerProperty timeSeconds = new SimpleIntegerProperty(startTime); //sets up binding
@@ -92,19 +94,6 @@ public class Pomodoro extends Program {
         });
         pomodoroScreen.getChildren().add(startButton);
 
-
-        /*
-        //configure vbox
-        VBox vb = new VBox(20);
-        vb.setAlignment(Pos.CENTER);
-        vb.setPrefWidth(scene.getWidth());
-        vb.setLayoutY(30);
-        vb.getChildren().addAll(button,timerLabel);
-        initialScreen.getChildren().add(vb);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-         */
     }
 
 
@@ -124,55 +113,21 @@ public class Pomodoro extends Program {
         changeGroup(currentGroup,initialScreen);
     }
 
+
+
+
+
+
+
+
     /*
-    @Override
-    public void start(Stage primaryStage){
-        //setup stage and scene
-        primaryStage.setTitle("Timer");
-        Scene scene = new Scene(initialScreen, 300,250);
-
-
-        //Label configuration
-        timerLabel.setText(timeMinutes.toString());
-        timerLabel.setTextFill(Color.RED);
-        timerLabel.setStyle("-fx-font-size: 4em;");
-        timerLabel.textProperty().bind(timeMinutes.asString()); //timerLabel shows current value of timeSeconds
-        //Binding: whenever the value of timeSeconds changes, the timerLabel text also changes
-
-        //button
-        Button button = new Button();
-        button.setText("Start");
-        button.setOnAction(actionEvent -> {
-            if(timeline != null){
-                timeline.stop();
-            }
-            timeMinutes.set(startTime);
-            timeline = new Timeline();
-            timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(startTime+1),new KeyValue(timeMinutes,0)));
-            timeline.playFromStart();
-
-        });
-        //configure vbox
-        VBox vb = new VBox(20);
-        vb.setAlignment(Pos.CENTER);
-        vb.setPrefWidth(scene.getWidth());
-        vb.setLayoutY(30);
-        vb.getChildren().addAll(button,timerLabel);
-        initialScreen.getChildren().add(vb);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-     */
-
-    /**
-     * Calc of min and sec
-     */
     private String secondsToString(int seconds) {
         int minutes = seconds / 60;
         seconds = seconds % 60;
         return String.format("%02d:%02d", minutes, seconds);
     }
+    
+     */
 
 
 }
