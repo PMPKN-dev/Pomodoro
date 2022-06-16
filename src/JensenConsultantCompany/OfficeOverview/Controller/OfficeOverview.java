49aeb40400fb699b31fbdbc55ca8b1da3374d7ab
Larskan <101105018+Larskan@users.noreply.github.com>
2022-06-16 20:54:55 +0200

    cleaned up a bit

--- src/JensenConsultantCompany/OfficeOverview/Controller/OfficeOverview.java
+++ src/JensenConsultantCompany/OfficeOverview/Controller/OfficeOverview.java
@@ -4,16 +4,14 @@ import JensenConsultantCompany.Foundation.Program;
 
 public class OfficeOverview extends Program {
 
-    //Experimenting with threading, this is just an experiment
-
-    public static void main(String[] args) {
-
-
-        MyRunnable consul1 = new MyRunnable();
-        Thread thread1 = new Thread(consul1);
-
-        thread1.start();
+    @Override
+    /**
+     * Overrides the expand within the Program Class
+     */
+    public void expand() {
+        
     }
+
 
 
 }
